62fdbad4cacfb015534287f22e06abaef2da7005
Roman Saykin <sc2aru@gmail.com>
2020-01-28 00:58:32 +0300

    organized folders and files

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    [SerializeField] private GameObject _firstObject;
+    [SerializeField] private GameObject _secondObject;
+
+    private Vector3 _startPosition;
+    private Vector3 _endPosition;
+    private Vector3 _positionDifference;
+    private Vector3 _sizeTemp;
+    private Vector3 _posTemp;
+
+    public void Init(GameObject firstCylinder, GameObject secondCyclinder)
+    {
+        _firstObject = firstCylinder;
+        _secondObject = secondCyclinder;
+    }
+}
