d5748d4df7767332f2e11a2fc3b62b6be170e200
Powertr1p <sc2aru@gmail.com>
2020-01-28 16:12:51 +0300

    added class to tracking is object in camera or not

---
+++ Assets/Scripts/TrackState.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackState : MonoBehaviour
+{
+    public bool FirstObjectIsSpotted { get; private set; }
+    public bool SecondIsObjectIsSpotted { get; private set; }
+    public bool ThirdIsObjectIsSpotted { get; private set; }
+
+    public void FirstSpotted()
+    {
+        FirstObjectIsSpotted = true;
+    }
+
+    public void SecondSpotted()
+    {
+        SecondIsObjectIsSpotted = true;
+    }
+
+    public void ThirdSpotted()
+    {
+        ThirdIsObjectIsSpotted = true;
+    }
+
+    public void FirstNotSpotted()
+    {
+        FirstObjectIsSpotted = false;
+    }
+
+    public void SecondNotSpotted()
+    {
+        SecondIsObjectIsSpotted = false;
+    }
+
+    public void ThirdNotSpotted()
+    {
+        ThirdIsObjectIsSpotted = false;
+    }
+}
--- Assets/Scripts/WallSpawner.cs
+++ Assets/Scripts/WallSpawner.cs
@@ -7,16 +7,33 @@ public class WallSpawner : MonoBehaviour
     [SerializeField] private GameObject _firstObject;
     [SerializeField] private GameObject _secondObject;
     [SerializeField] private GameObject _wallPrefab;
+    [SerializeField] private TrackState _detector;
 
     private bool _isSpawned;
 
-    private void Update()
+    private void Start()
     {
-        if (!_isSpawned)
+        _detector = GetComponent<TrackState>();
+    }
+
+    public void SpawnWall()
+    {
+        if (!_isSpawned && _detector.FirstObjectIsSpotted && _detector.SecondIsObjectIsSpotted)
         {
             var wall = Instantiate(_wallPrefab, Vector3.zero, Quaternion.identity) as GameObject;
             wall.GetComponent<Wall>().Init(ref _firstObject, ref _secondObject);
+            wall.transform.SetParent(transform);
             _isSpawned = true;
+        }
+    }
+
+    public void DestroyWall()
+    {
+        if (_isSpawned)
+        {
+            var wall = GetComponentInChildren<Wall>();
+            Destroy(wall.gameObject);
+            _isSpawned = false;
         }
     }
 }
