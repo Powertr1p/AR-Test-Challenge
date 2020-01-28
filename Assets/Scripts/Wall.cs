fd31dfd9d2edb398043fb41f62d91872472d6fd7
Powertr1p <sc2aru@gmail.com>
2020-01-28 15:16:33 +0300

    created scaling wall and wallspawner

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class Wall : MonoBehaviour
 {
-    [SerializeField] private GameObject _firstObject;
-    [SerializeField] private GameObject _secondObject;
+    private GameObject _first;
+    private GameObject _second;
 
     private Vector3 _startPosition;
     private Vector3 _endPosition;
-    private Vector3 _positionDifference;
-    private Vector3 _sizeTemp;
-    private Vector3 _posTemp;
 
-    public void Init(GameObject firstCylinder, GameObject secondCyclinder)
+    private Vector3 _distanceBetweenObjects;
+    private Vector3 _positionInMiddle;
+
+    public void Init(ref GameObject firstObject, ref GameObject secondObject)
     {
-        _firstObject = firstCylinder;
-        _secondObject = secondCyclinder;
+        _first = firstObject;
+        _second = secondObject;
+    }
+
+    private void Update()
+    {
+        GetNewPositionPoints();
+        UpdateCurrentPosition();
+    }
+
+    private void GetNewPositionPoints()
+    {
+        _startPosition = _first.transform.position;
+        _endPosition = _second.transform.position;
+
+        _distanceBetweenObjects = _endPosition - _startPosition;
+        _positionInMiddle = (_distanceBetweenObjects) / 2 + _startPosition;
+    }
+
+    private void UpdateCurrentPosition()
+    {
+        transform.position = _positionInMiddle;
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, _distanceBetweenObjects);
+        transform.localScale = new Vector3(transform.localScale.x, _distanceBetweenObjects.magnitude * 1, transform.localScale.z);
     }
 }
--- Assets/Scripts/WallSpawner.cs
+++ Assets/Scripts/WallSpawner.cs
@@ -4,4 +4,19 @@ using UnityEngine;
 
 public class WallSpawner : MonoBehaviour
 {
+    [SerializeField] private GameObject _firstObject;
+    [SerializeField] private GameObject _secondObject;
+    [SerializeField] private GameObject _wallPrefab;
+
+    private bool _isSpawned;
+
+    private void Update()
+    {
+        if (!_isSpawned)
+        {
+            var wall = Instantiate(_wallPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+            wall.GetComponent<Wall>().Init(ref _firstObject, ref _secondObject);
+            _isSpawned = true;
+        }
+    }
 }
